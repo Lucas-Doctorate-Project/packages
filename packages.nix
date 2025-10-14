b98cc86ee499da2f1e23adfba2e7ade704fc8b2a
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2025-10-14 17:19:00 -0300

    add remaining packages to use batsim

--- packages.nix
+++ packages.nix
@@ -1,20 +1,34 @@
 {
   pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
-    sha256 = "sha256:06jzngg5jm1f81sc4xfskvvgjy5bblz51xpl788mnps1wrkykfhp";
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/nixos-25.05.tar.gz";
+    sha256 = "sha256-4xggC56Rub3WInz5eD7EZWXuLXpNvJiUPahGtMkwtuc=";
   }) {}
 }:
 
 with pkgs;
 
 rec {
+  batexpe = callPackage ./pkgs/batexpe {};
+
+  batsched-140 = callPackage ./pkgs/batsched/batsched140.nix { inherit loguru redox; intervalset = intervalset-light; };
+  batsched = batsched-140;
+
   batsim = batsim420;
   batsim420 = callPackage ./pkgs/batsim/batsim420.nix { inherit redox; simgrid = simgrid-light; intervalset = intervalset-light; };
 
-  intervalset = callPackage ./pkgs/intervalset { };
+  intervalset = callPackage ./pkgs/intervalset {};
   intervalset-light = intervalset.override { withoutBoostPropagation = true; };
+
+  loguru = callPackage ./pkgs/loguru {};
+
+  procset = callPackage ./pkgs/procset {};
+
+  pybatsim-core = pybatsim-core-400;
+  pybatsim-functional = pybatsim-functional-400;
+  pybatsim-core-400 = callPackage ./pkgs/pybatsim/core400.nix { inherit procset; };
+  pybatsim-functional-400 = callPackage ./pkgs/pybatsim/functional400.nix { pybatsim-core = pybatsim-core-400; };
   
-  redox = callPackage ./pkgs/redox { };
+  redox = callPackage ./pkgs/redox {};
   
   simgrid = simgrid-400;
   simgrid-light = simgrid-400light;
